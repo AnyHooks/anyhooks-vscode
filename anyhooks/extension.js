2862e075e32e4288b015b5fd0dedf89343cd2573
Malnati <ricardomalnati@gmail.com>
2023-11-07 23:49:18 +0100

    feat: adding yo code gen files

---
+++ anyhooks/extension.js
@@ -0,0 +1,31 @@
+const vscode = require('vscode');
+const { exec } = require('child_process');
+
+function activate(context) {
+  let disposable = vscode.workspace.onDidChangeTextDocument(event => {
+    if (event.contentChanges.length > 0) {
+      // Assuming you have a bash script at the root of your project named 'script.sh'
+      exec('bash ./script.sh', (err, stdout, stderr) => {
+        if (err) {
+          // Handle error case
+          console.error(`Execution error: ${err}`);
+          return;
+        }
+        // Log the script output
+        console.log(`stdout: ${stdout}`);
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+        }
+      });
+    }
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
